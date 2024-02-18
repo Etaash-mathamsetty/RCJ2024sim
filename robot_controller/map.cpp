b8f663c05dda1cd03cd1421d0fff052c27eca27a
ChineseChen <jiachen.jiang2016@gmail.com>
2024-02-18 17:55:44 -0500

    camera point mapping

--- robot_controller/map.cpp
+++ robot_controller/map.cpp
@@ -8,6 +8,11 @@
 
 #include "map.h"
 
+#define PI 3.141592653589793238462643383
+#define pdd std::pair<double, double>
+#define f first
+#define s second
+
 using namespace webots;
 
 const double region_size = 0.1;
@@ -16,13 +21,13 @@ const double region_size = 0.1;
 template <class T>
 inline void hash_combine(std::size_t & s, const T & v)
 {
-  std::hash<T> h;
-  s ^= h(v) + 0x9e3779b9 + (s<< 6) + (s>> 2);
+    std::hash<T> h;
+    s ^= h(v) + 0x9e3779b9 + (s<< 6) + (s>> 2);
 }
 
 inline bool nearly_equal(double a, double b, const double thresh = 0.02)
 {
-  return std::abs(a-b) <= thresh;
+    return std::abs(a-b) <= thresh;
 }
 
 inline double round_to(double value, const double precision = 0.01)
@@ -37,21 +42,21 @@ inline double floor_to(double value, const double precision = 0.01)
 
 struct GPS_position
 {
-  double x;
-  int y; //floor number
-  double z;
+    double x;
+    int y; //floor number
+    double z;
 
-  GPS_position(const double *pos)
-  {
-    x = *pos;
-    y = *(pos+1);
-    z = *(pos+2);
-  }
+    GPS_position(const double *pos)
+    {
+        x = *pos;
+        y = *(pos+1);
+        z = *(pos+2);
+    }
 
-  bool operator==(const GPS_position& other) const
-  {
-    return nearly_equal(other.x, x) && other.y == y && nearly_equal(other.z, z);
-  }
+    bool operator==(const GPS_position& other) const
+    {
+        return nearly_equal(other.x, x) && other.y == y && nearly_equal(other.z, z);
+    }
 
 };
 
@@ -60,16 +65,16 @@ struct GPS_hash
     std::size_t operator()(const GPS_position& pos) const
     {
         std::size_t res = 0;
-        hash_combine(res,pos.x);
-        hash_combine(res,pos.y);
-        hash_combine(res,pos.z);
+        hash_combine(res, pos.x);
+        hash_combine(res, pos.y);
+        hash_combine(res, pos.z);
         return res;
     }
 };
 
 struct REGION {
-  //points
-  std::map<std::pair<double,double>, bool> points;
+    //points
+    std::map<std::pair<double,double>, bool> points;
 };
 
 std::unordered_map<GPS_position, REGION, GPS_hash> regions;
@@ -78,75 +83,192 @@ std::vector<std::pair<double, double>> vec_points;
 //theta is in radians
 void update_regions_map(GPS *gps, const float *lidar_image, float theta)
 {
-  vec_points.reserve(20000);
+    vec_points.reserve(20000);
 
-  for(int i = 0; i < 512; i++)
-  {
-    const float dist = lidar_image[i];
-    if(std::isinf(dist))
-        continue;
-    const float angle = i/512.0 * 2.0 * M_PI;
-    double pos[3];
-    double pos_rounded[3];
-    memcpy(pos, gps->getValues(), 3 * sizeof(double));
-    pos[2] *= -1;
-    //force single floor for now
-    pos[1] = 0;
-    pos_rounded[0] = floor_to(pos[0], region_size);
-    pos_rounded[1] = floor_to(pos[1], region_size);
-    pos_rounded[2] = floor_to(pos[2], region_size);
-    double x = dist*sin(angle - theta) + (pos[0] - pos_rounded[0]);
-    double y = dist*cos(angle - theta) + (pos[2] - pos_rounded[2]);
-
-    x = round_to(x);
-    y = round_to(y);
-
-    while(x < 0)
+    for(int i = 0; i < 512; i++)
     {
-      x += region_size;
-      pos_rounded[0] -= region_size;
-    }
+        const float dist = lidar_image[i];
+        if(std::isinf(dist))
+            continue;
+        const float angle = i/512.0 * 2.0 * PI;
+        double pos[3];
+        double pos_rounded[3];
+        memcpy(pos, gps->getValues(), 3 * sizeof(double));
+        pos[2] *= -1;
+        //force single floor for now
+        pos[1] = 0;
+        pos_rounded[0] = floor_to(pos[0], region_size);
+        pos_rounded[1] = floor_to(pos[1], region_size);
+        pos_rounded[2] = floor_to(pos[2], region_size);
+        double x = dist*sin(angle - theta) + (pos[0] - pos_rounded[0]);
+        double y = dist*cos(angle - theta) + (pos[2] - pos_rounded[2]);
 
-    while(y < 0)
+        x = round_to(x);
+        y = round_to(y);
+
+        while(x < 0)
+        {
+            x += region_size;
+            pos_rounded[0] -= region_size;
+        }
+
+        while(y < 0)
+        {
+            y += region_size;
+            pos_rounded[2] -= region_size;
+        }
+
+        while(y >= region_size)
+        {
+            y -= region_size;
+            pos_rounded[2] += region_size;
+        }
+
+        while(x >= region_size)
+        {
+            x -= region_size;
+            pos_rounded[0] += region_size;
+        }
+
+        x = round_to(x);
+        y = round_to(y);
+
+        const auto coord = std::make_pair(x,y);
+        //std::cout << "x: " << x << " y: " << y << std::endl;
+        const auto coord2 = std::make_pair(x + pos_rounded[0], y + pos_rounded[2]);
+        if(regions[GPS_position(pos_rounded)].points.count(coord) == 0)
+        {
+            vec_points.push_back(coord2);
+            regions[GPS_position(pos_rounded)].points[coord] = true;
+        }
+    }
+}
+
+double clampAngle(double theta)
+{
+    if(abs(theta) > PI)
     {
-      y += region_size;
-      pos_rounded[2] -= region_size;
+        while(abs(theta) > PI)
+        {
+            theta -= copysign(2, theta) * PI;
+        }
     }
+    return theta;
+}
 
-    while(y >= region_size)
+bool isBetween(double theta, double start, double end)
+{
+    end -= start;
+    theta -= start;
+    if(theta < 0)
     {
-      y -= region_size;
-      pos_rounded[2] += region_size;
+        theta += 2 * PI;
     }
-
-    while(x >= region_size)
+    if(end < 0)
     {
-      x -= region_size;
-      pos_rounded[0] += region_size;
+        end += 2 * PI;
     }
+    return theta <= end;
+}
 
-    x = round_to(x);
-    y = round_to(y);
+std::vector<std::pair<double, double>> cameraPoints;
 
-    const auto coord = std::make_pair(x,y);
-    //std::cout << "x: " << x << " y: " << y << std::endl;
-    const auto coord2 = std::make_pair(x + pos_rounded[0], y + pos_rounded[2]);
-    if(regions[GPS_position(pos_rounded)].points.count(coord) == 0)
+void update_camera_map(GPS *gps, const float *lidar_image, float theta, Camera* camera)
+{
+    cameraPoints.reserve(20000);
+    double fov = camera->getFov(); //radians
+
+    double leftAngle = clampAngle(theta - PI / 2);
+    double rightAngle = clampAngle(theta + PI / 2);
+
+    //sweeping counterclockwise
+    pdd leftEndpoints = pdd(clampAngle(leftAngle + fov / 2),
+        clampAngle(leftAngle - fov / 2));
+    pdd rightEndpoints = pdd(clampAngle(rightAngle + fov / 2),
+        clampAngle(rightAngle - fov / 2));
+
+    cameraPoints.reserve(20000);
+
+    for(int i = 0; i < 512; i++)
     {
-        vec_points.push_back(coord2);
-        regions[GPS_position(pos_rounded)].points[coord] = true;
+        const float dist = lidar_image[i];
+        if(std::isinf(dist))
+            continue;
+        const float angle = clampAngle(i/512.0 * 2.0 * PI);
+        if(!isBetween(angle, leftEndpoints.f, leftEndpoints.s) &&
+            !isBetween(angle, rightEndpoints.f, rightEndpoints.s))
+            continue;
+        double pos[3];
+        double pos_rounded[3];
+        memcpy(pos, gps->getValues(), 3 * sizeof(double));
+        pos[2] *= -1;
+        //force single floor for now
+        pos[1] = 0;
+        pos_rounded[0] = floor_to(pos[0], region_size);
+        pos_rounded[1] = floor_to(pos[1], region_size);
+        pos_rounded[2] = floor_to(pos[2], region_size);
+        double x = dist*sin(angle - theta) + (pos[0] - pos_rounded[0]);
+        double y = dist*cos(angle - theta) + (pos[2] - pos_rounded[2]);
+
+        x = round_to(x);
+        y = round_to(y);
+
+        while(x < 0)
+        {
+            x += region_size;
+            pos_rounded[0] -= region_size;
+        }
+
+        while(y < 0)
+        {
+            y += region_size;
+            pos_rounded[2] -= region_size;
+        }
+
+        while(y >= region_size)
+        {
+            y -= region_size;
+            pos_rounded[2] += region_size;
+        }
+
+        while(x >= region_size)
+        {
+            x -= region_size;
+            pos_rounded[0] += region_size;
+        }
+
+        x = round_to(x);
+        y = round_to(y);
+
+        const auto coord = std::make_pair(x,y);
+        //std::cout << "x: " << x << " y: " << y << std::endl;
+        const auto coord2 = std::make_pair(x + pos_rounded[0], y + pos_rounded[2]);
+        if(regions[GPS_position(pos_rounded)].points.count(coord) == 0)
+        {
+            cameraPoints.push_back(coord2);
+            regions[GPS_position(pos_rounded)].points[coord] = true;
+        }
     }
-  }
+}
+
+std::vector<std::pair<double, double>> getLidarPoints()
+{
+    return vec_points;
+}
+
+std::vector<std::pair<double, double>> getCameraPoints()
+{
+    return cameraPoints;
 }
 
 ImPlotPoint getPointFromMap(int idx, void *_map)
 {
-  return ImPlotPoint(vec_points[idx].first, vec_points[idx].second);
+    return ImPlotPoint(vec_points[idx].first, vec_points[idx].second);
 }
 
 size_t getCount()
 {
-  return vec_points.size();
+    return vec_points.size();
 }
 
 void clearPointCloud()
--- robot_controller/map.h
+++ robot_controller/map.h
@@ -1,5 +1,6 @@
 #include <webots/Robot.hpp>
 #include <webots/GPS.hpp>
+#include <webots/Camera.hpp>
 
 void plotPoints(int w, int h, bool plot_regions=true);
 void update_regions_map(webots::GPS *gps, const float *lidar_image, float theta=0);
