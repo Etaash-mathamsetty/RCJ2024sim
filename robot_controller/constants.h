af8dd532733a6fe5e09609fef1d8c0c1366fc006
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2024-03-21 23:20:08 -0400

    add M_PI to constants.h

--- robot_controller/constants.h
+++ robot_controller/constants.h
@@ -12,3 +12,7 @@ enum class DIR
 #define M_PER_TILE 0.135
 #define MAX_VIC_DETECTION_RANGE 0.08
 #define CAMERA_FOV 1 //yep that's the actual number
+
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643383
+#endif
