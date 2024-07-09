993a5f8a53439584049a59bc3ba7422dcc019f5d
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-07-09 19:19:03 -0400

    fix compile bug

--- robot_controller/RobotInstance.cpp
+++ robot_controller/RobotInstance.cpp
@@ -315,7 +315,7 @@ RobotInstance::RobotInstance()
 
     update_lidar_cloud();
     bfsAddOnWall(getCurrentGPSPosition(), 0.4);
-    initGrid();
+    initGrid(r3d(getRawGPSPosition()));
 }
 
 RobotInstance::~RobotInstance()
