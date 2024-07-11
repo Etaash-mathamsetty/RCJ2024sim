8654e534436a840818730ff5702f6e61576e3ef9
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-07-11 18:48:07 -0400

    reduce min contour area for O and F hazard

--- robot_controller/RobotInstance.cpp
+++ robot_controller/RobotInstance.cpp
@@ -792,7 +792,7 @@ char RobotInstance::checkHsv(cv::Mat roi, std::string side)
         double asp_ratio = rect.width / (double)rect.height;
         if(asp_ratio <= 0.8 || asp_ratio >= 3.0)
             continue;
-        if(area >= 50 && area <= 3500 && (big_orange_c.size() == 0 || area > cv::contourArea(big_orange_c)))
+        if(area >= 40 && area <= 3500 && (big_orange_c.size() == 0 || area > cv::contourArea(big_orange_c)))
             big_orange_c = orange_c[i];
     }
 
@@ -803,7 +803,7 @@ char RobotInstance::checkHsv(cv::Mat roi, std::string side)
         double asp_ratio = rect.width / (double)rect.height;
         if(asp_ratio <= 0.8 || asp_ratio >= 3.0)
             continue;
-        if(area >= 50 && area <= 3500 && (big_red_c.size() == 0 || area > cv::contourArea(big_red_c)))
+        if(area >= 40 && area <= 3500 && (big_red_c.size() == 0 || area > cv::contourArea(big_red_c)))
             big_red_c = red_c[i];
     }
 
