e1d53c079d3cee3115db07b1a14fe68cf5f56589
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-07-11 01:13:19 -0400

    Revert "try to fix dfsWallTrace"
    This reverts commit b25b51e73835cbd4cc5a0a5625ffcd966b3aa174.

--- robot_controller/navigation.cpp
+++ robot_controller/navigation.cpp
@@ -32,7 +32,7 @@ double getDist2(const pdd& pt1, const pdd& pt2)
     return pow(pt2.f - pt1.f, 2) + pow(pt2.s - pt1.s, 2);
 }
 
-bool compPts(const pdd& pt1, const pdd& pt2, double thresh)
+inline bool compPts(const pdd& pt1, const pdd& pt2, double thresh)
 {
     return (abs(pt1.f - pt2.f) < thresh && abs(pt1.s - pt2.s) < thresh);
 }
@@ -789,19 +789,18 @@ stack<pdd> dfsWallTrace(RobotInstance* rb, pdd _cur)
         wallNode node = st.top();
         pdd point = node.point;
         double rotation = node.direction;
-        bool midpoint = midpoint_check(cur, point);
         st.pop();
-        if (isOnWall(point) && (point.f < min.f || point.f > max.f || point.s < min.s || point.s > max.s) && midpoint)
+        if (isOnWall(point) && (point.f < min.f || point.f > max.f || point.s < min.s || point.s > max.s))
         {
             isFound = true;
             tar = point;
             break;
         }
-        if (!midpoint || visited.count(point) > 0 || !isOnWall(point) || ((isVisited(point) || isPseudoVisited(point)) && point != cur) || point.f < min.f || point.f > max.f || point.s < min.s || point.s > max.s)
+        if (visited.count(point) > 0 || !isOnWall(point) || ((isVisited(point) || isPseudoVisited(point)) && point != cur) || point.f < min.f || point.f > max.f || point.s < min.s || point.s > max.s)
         {
             continue;
         }
-        if (abs(rotation) > M_PI / 4 + 0.05 && midpoint)
+        if (abs(rotation) > M_PI / 4 + 0.05)
         {
             isFound = true;
             tar = point;
